2c9a5c805525d194409df0bb4dbe127e5c4484ab
Murilo Nascimento <murilojesus659@gmail.com>
2024-02-12 15:42:29 -0300

    feature: implement repository and convert methods to class

--- src/controllers/CustomerController.ts
+++ src/controllers/CustomerController.ts
@@ -1,46 +1,50 @@
 import { Request, Response } from "express";
-import { createCustomer, deleteById, findAllCustomers, updateById } from "../services/CustomerService";
+import CustomerService from "../services/CustomerService";
 
-export async function getAll(req: Request, res: Response) {
-  try {
-    const customers = await findAllCustomers();
-
-    return res.status(200).json(customers);
-  } catch (error) {
-    return res.status(500).json(error);
+class CustomerController {
+  async getAll(req: Request, res: Response) {
+    try {
+      const customers = await CustomerService.findAllCustomers();
+  
+      return res.status(200).json(customers);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  }
+  
+  async postCustomer(req: Request, res: Response) {
+    try {
+      const newCustomer = await CustomerService.createCustomer(req.body);
+  
+      return res.status(201).json(newCustomer);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  }
+  
+  async updateCustomer(req: Request, res: Response) {
+    try {
+      await CustomerService.updateById(req.params.id, req.body);
+  
+      return res.status(200).json({
+        message: `Customer ID ${req.params.id} updated successfully.`
+      });
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  }
+  
+  async deleteCustomer(req: Request, res: Response) {
+    try {
+      await CustomerService.deleteById(req.params.id);
+  
+      return res.status(200).json({
+        message: `Customer ID ${req.params.id} removed successfully.`
+      });
+    } catch (error) {
+      return res.status(500).json(error);
+    }
   }
 }
 
-export async function postCustomer(req: Request, res: Response) {
-  try {
-    const newCustomer = await createCustomer(req.body);
-
-    return res.status(201).json(newCustomer);
-  } catch (error) {
-    return res.status(500).json(error);
-  }
-}
-
-export async function updateCustomer(req: Request, res: Response) {
-  try {
-    await updateById(req.params.id, req.body);
-
-    return res.status(200).json({
-      message: `Customer ID ${req.params.id} updated successfully.`
-    });
-  } catch (error) {
-    return res.status(500).json(error);
-  }
-}
-
-export async function deleteCustomer(req: Request, res: Response) {
-  try {
-    await deleteById(req.params.id);
-
-    return res.status(200).json({
-      message: `Customer ID ${req.params.id} removed successfully.`
-    });
-  } catch (error) {
-    return res.status(500).json(error);
-  }
-}
+export default new CustomerController;
